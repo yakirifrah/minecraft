af14b4b40a1c704a2521273ddd667d75fef10894
yakir <yakirifrah19@gmail.com>
2019-06-20 14:54:02 +0300

    nada

--- js/mincraft.js
+++ js/mincraft.js
@@ -7,10 +7,10 @@ class Minecraft {
         this.tool.setTool(tool);
     }
     mine(e) {
-        console.log(e.target.getAttribute("data"));
+        // console.log(e.target.getAttribute("data"));
         let element = e.target.getAttribute("data");
-
-
+        let cuurentTool = this.tool.getTool();
+        console.log(cuurentTool);
     }
 }
 
