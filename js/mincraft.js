33085a34da916e26c1ac71b5d57b96e831fa1c69
outboxltd <benatiya@gmail.com>
2019-06-20 12:41:28 +0300

    sameTool

--- js/mincraft.js
+++ js/mincraft.js
@@ -6,3 +6,13 @@ class Mincraft {
         this.world.tools[3]().type
     }
 }
+
+
+function sameTool(element,tool) {
+    if(element === tool) {
+        return true;
+    } else {
+        $(tool).css("border-color", "red");
+        return false;
+    }
+} 
