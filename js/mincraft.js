29cdef30ad061f86c7396e2a7da88418fdda3947
yakir <yakirifrah19@gmail.com>
2019-06-20 20:51:29 +0300

    befor conflict

--- js/mincraft.js
+++ js/mincraft.js
@@ -10,8 +10,6 @@ class Minecraft {
         if (element === tool) {
             return true;
         } else {
-            // console.log("this is a element " + element);
-            // console.log("this is a tool " + tool);
             let toolBox = document.getElementsByClassName(tool);
             $(toolBox).toggleClass("mistakeBorder");
             setTimeout(function () {
@@ -23,11 +21,13 @@ class Minecraft {
     }
     mine(e) {
         let div = e.target;
-        console.log(div);
         let element = e.target.getAttribute("data");
         let tool = game.tool.getTool();
         let res = $(div).hasClass(`box sky`);
         if (game.sameTool(element, tool) && !res) {
+            game.tool.currentMining.push(div.classList[0]);
+            console.log(object);
+            $('.CurrentResource').addClass(game.tool.currentMining.pop());
             $(div).removeClass(div.classList[0]);
             $(div).addClass("sky");
         }
