546362e69c34216587a5103313402878db972ee0
yakir <yakirifrah19@gmail.com>
2019-06-20 15:23:10 +0300

    finish mine function,bug is fixed

--- js/mincraft.js
+++ js/mincraft.js
@@ -10,8 +10,8 @@ class Minecraft {
         if (element === tool) {
             return true;
         } else {
-            console.log("this is a element " + element);
-            console.log("this is a tool " + tool);
+            // console.log("this is a element " + element);
+            // console.log("this is a tool " + tool);
             let toolBox = document.getElementsByClassName(tool);
             $(toolBox).toggleClass("mistakeBorder");
             setTimeout(function () {
@@ -23,11 +23,11 @@ class Minecraft {
     }
     mine(e) {
         let div = e.target;
+        console.log(div);
         let element = e.target.getAttribute("data");
         let tool = game.tool.getTool();
-        if (game.sameTool(element, tool)) {
-            // console.log(div)
-            // console.log(element);
+        let res = $(div).hasClass(`box sky`);
+        if (game.sameTool(element, tool) && !res) {
             $(div).removeClass(div.classList[0]);
             $(div).addClass("sky");
         }
