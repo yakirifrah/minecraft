49bde230aad66afbd9befe62e2a712e5ee06d695
outboxltd <benatiya@gmail.com>
2019-06-22 19:51:54 +0300

    new staff

---
+++ tutorial.js
@@ -0,0 +1,14 @@
+$(document).ready(function () { $("#tools").hide(), $("#world").hide() });
+
+$("#btn_tools").click(function () {
+    $("#tools").toggle("highlight");
+});
+
+$("#btn_world").click(function () {
+    $("#world").toggle("highlight");
+});
+
+
+// $(document).ready(function(){       
+//     $('#the_gilad').show();
+//      }); 
